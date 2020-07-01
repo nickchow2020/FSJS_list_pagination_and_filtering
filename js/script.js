91cca758da45587761171ac00d58185b3e6e6b5a
Stephenchow <39304873+stephencow@users.noreply.github.com>
2020-07-01 14:39:31 -0700

    meets

--- js/script.js
+++ js/script.js
@@ -7,44 +7,124 @@ FSJS project 2 - List Filter and Pagination
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+ *global Variable That We need for This project
 ***/
+        // variable that target on students Ul tag
+const studentsUl = document.querySelector(".student-list");
+        // variable that store all list element under the students Ul tags
+const studentsUlList = studentsUl.children;
+        // initial the current page to one;
+const currentPage = 1;
+        // initial the rows students per page;
+const rowsPerPage = 10;
+        // target on the parent of pagination section;
+const page = document.querySelector(".page")
 
 
 
 
 /*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
+ *function that is going to who the only 10 rows perpages and hide the rest
+    **page argument is represent the current page
 ***/
+
+function showPage(page){
+        // making sure to empty the previous contents used for loops
+        const listLength = studentsUlList.length;//set highest index in for loops
+
+        for(let i = 0; i < listLength; i ++){
+                studentsUlList[i].style.display = "";
+        }
+
+        page -- // set the page to minus one,making the start index position is begin at 0;
+        const startIndex = page * rowsPerPage;  // set the start index
+        const endIndex = startIndex + rowsPerPage; // set the end index
+
+        /**use for loop to loop through each index according the variable startIndex and endIndex and 
+        set it's display property to block;**/
+        for(let i = startIndex; i < endIndex; i ++){
+                /**if statement filters studentsUlList[i] equivalent to undefined*/
+                if(studentsUlList[i]){ 
+                        studentsUlList[i].style.display = "block";
+                }
+        }
+}
+
 
 
 
 
 /*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
+ *function that is going to generate the Pagination Buttons and it's functionalities
+ btnNumber is the button's contents like 1,2,3,4,5,6
 ***/
+function createPaginationBtn(btnNumber){
+        let page = btnNumber + 1 // because we start the for loop at index of 0,making sure the buttons number no equivalent to 0
+        const li = document.createElement("li");// create li element;
+        const a = document.createElement("a");// create anchor tags;
+        a.textContent = page; // set anchor tags contents to the current_page;
+        li.appendChild(a); // append anchor tag to li tag;
+
+        if(currentPage === page)a.classList.add("active"); // add the class active to first btn used conditional statement
+
+        /**
+         * Add an Eventlistener to each anchor tags,when click run the function showPage() and remove and add the 
+         * class active to button when click.
+         */
+        a.addEventListener("click",(e)=>{
+                const buttonContents = e.target.textContent;// get the showpage() argument value
+                const activeBtn = document.querySelector(".active") // select the anthor tag with class of active
+                activeBtn.classList.remove("active"); // remove class active when click
+                showPage(buttonContents);// call the showPage() and pass it's argument value when click
+                a.classList.add("active");// add  class active when click a tags
+        })
 
 
 
 
+        return li; // return the value of the function li tag;
+}
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+
+
+
+/*** 
+ *function that is going to generate the pagination buttons according to the length of the students.
+ and called a function that is handle the funtionally of each button.
+ parent is argument wrapper of pagination section.
+***/
+function appendPaginateBtn(parent){
+        /*** 
+         * first step going to create a html of pagination
+         * ***/
+        const div = document.createElement("div");
+        const ul = document.createElement("ul");
+        div.classList.add("pagination");
+        div.appendChild(ul);
+        parent.appendChild(div);
+
+        /**variable:
+         * Use Math.ceil to level up the numbers of buttons
+         * use students total divide by the rowsPerpages
+         */
+        const totalStudents =  studentsUlList.length;  //totals student value
+        const totalBtn = Math.ceil(totalStudents / rowsPerPage) // total buttons
+
+        /**
+         * Use for loop to loop throught the numbers of pagination and called the 
+         * function of createPaginationBtn.
+         */
+        for(let i = 0;i < totalBtn; i ++){
+                let paginated = createPaginationBtn(i);// store each li tags
+                ul.appendChild(paginated); // append each li tags to ul element
+        }
+
+
+
+}
+
+
+
+showPage(1);
+appendPaginateBtn(page);
+    
